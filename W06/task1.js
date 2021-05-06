935fc338ebacc1ecb212e72a17b40d42906d2ad2
mikan1996 <65080003+mikan1996@users.noreply.github.com>
2021-05-06 15:00:37 +0900

    add new file

--- W06/task1.js
+++ W06/task1.js
@@ -1,4 +1,4 @@
-d3.csv("https://vizlab-kobe-lecture.github.io/InfoVis2021/W04/data.csv")
+d3.csv("https://mikan1996.github.io/InfoVis2021/W04/task1.csv")
     .then( data => {
         data.forEach( d => { d.x = +d.x; d.y = +d.y; });
 
